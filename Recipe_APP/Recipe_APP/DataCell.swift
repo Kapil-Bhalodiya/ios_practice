0b5eca98d29ed2432c6e8f5d985f86b3fdfb5251
Kapil-Bhalodiya <66879974+Kapil-Bhalodiya@users.noreply.github.com>
2021-12-04 17:34:13 +0530

    4-12-21
    edit 1

--- Recipe_APP/Recipe_APP/DataCell.swift
+++ Recipe_APP/Recipe_APP/DataCell.swift
@@ -17,6 +17,7 @@ class DataCell: UITableViewCell {
     
     private let content:UILabel={
         let lbl = UILabel()
+        lbl.numberOfLines = 3
         return lbl
     }()
     
@@ -25,8 +26,8 @@ class DataCell: UITableViewCell {
         contentView.addSubview(title)
         contentView.addSubview(content)
         myimgview.frame = CGRect(x:20,y:20,width: 80,height: 100)
-        title.frame = CGRect(x:myimgview.right + 20 , y:50,width: 200,height: 20)
-        content.frame = CGRect(x:myimgview.right + 20, y:20,width: 200,height: contentView.height)
+        title.frame = CGRect(x:myimgview.right + 20 , y:30,width: 200,height: 20)
+        content.frame = CGRect(x:myimgview.right + 20, y:10,width: contentView.width-120,height: contentView.height)
         myimgview.image = UIImage(named: name)
         title.text = name
         content.text = con
